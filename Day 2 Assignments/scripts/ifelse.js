e9f8addac28560dd372d097b13b5fd44014785e8
ThamizhThileepan <63794824+ThamizhThileepan@users.noreply.github.com>
2020-07-16 12:31:00 +0530

    Day 2 Assignments

---
+++ Day 2 Assignments/scripts/ifelse.js
@@ -0,0 +1,7 @@
+var user = confirm("Are you above ?");
+if (user == true) {
+    console.log("Can Drink");
+}
+else {
+    console.log("Cannot Drink");
+}
---
+++ Day 2 Assignments/scripts/input.js
@@ -0,0 +1,4 @@
+var degree = prompt("Please enter your age"+);
+if (degree != null) {
+    console.log("My age is "+degree+" for the registration.");
+}
