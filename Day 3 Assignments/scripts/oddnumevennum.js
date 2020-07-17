4b6705e2405c0dbfe2121d03a175a0f678558b6d
ThamizhThileepan <63794824+ThamizhThileepan@users.noreply.github.com>
2020-07-17 11:46:37 +0530

    Day 3 Assignments

---
+++ Day 3 Assignments/scripts/oddnumevennum.js
@@ -0,0 +1,8 @@
+let number = prompt("Enter your number :");
+let res = number%2;
+if (res == 0) {
+    console.log(`The number entered is ${number} and the number is even`);
+}
+else {
+    console.log(`The number entered is ${number} and the number is odd`);
+}
---
+++ Day 3 Assignments/scripts/osversion.js
@@ -0,0 +1,4 @@
+let os = prompt("Enter your OS name :");
+let version = prompt("Enter your version of the OS :");
+console.log(`${os} ${version}`);
+console.log(`The OS name is ${os} and the version is ${version}`);
